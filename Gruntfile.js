89555d3a801687ddb779d6a59c9732a3ca139e67
davidsmith2 <davidsmith2@gmail.com>
2015-02-11 00:30:30 -0500

    complete LESS port, standardize output of compiled CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,44 @@ module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
-      build: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
         files: {
           'dist/sass/helpers.css': 'src/sass/helpers.scss'
         }
       }
     },
     less: {
-      development: {
+      dist: {
         options: {
-          paths: ['src/less']
+          paths: ['src/less'],
+          sourceMap: true,
+          sourceMapFilename: 'dist/less/helpers.css.map',
+          sourceMapURL: 'helpers.css.map'
         },
         files: {
           'dist/less/helpers.css': 'src/less/helpers.less'
         }
+      }
+    },
+    replace: {
+      sass: {
+        src: ['dist/sass/helpers.css'],
+        overwrite: true,
+        replacements: [
+          {
+            from: /^\n/gm,
+            to: function () {
+              return;
+            }
+          },
+          {
+            from: /(,)\s/gm,
+            to: '$1\n'
+          }
+        ]
       }
     },
     exec: {
@@ -30,21 +54,21 @@ module.exports = function(grunt){
     watch: {
       sass: {
         files: [
-        'src/sass/*.scss',
-        'src/sass/**/*.scss'
+          'src/sass/*.scss',
+          'src/sass/**/*.scss'
         ],
         tasks: ['build-sass']
       },
       less: {
         files: [
-        'src/less/*.less',
-        'src/less/**/*.less'
+          'src/less/*.less',
+          'src/less/**/*.less'
         ],
         tasks: ['build-less']
       }
-    },
+    }
   });
   grunt.registerTask('default', ['build-sass','build-less','watch']);
-  grunt.registerTask('build-sass',  ['sass','exec:minsass']);
+  grunt.registerTask('build-sass',  ['sass','replace:sass','exec:minsass']);
   grunt.registerTask('build-less',  ['less','exec:minless']);
 };
